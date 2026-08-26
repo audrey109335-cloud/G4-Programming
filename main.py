c924d0ccd6012f8d8d7752acd9df98140dffe398
audrey109335 <audrey109335@mcsva.org>
2026-08-26 11:54:20 -0400

    .

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ Filename: interview.py
 Author: <Francsico, Audrey>
 Created: <MM/DD/YYYY>
 Instructor: Holtslander
-"""
 input("What is your name?")
+"""
 input("What made you want to work at Osbourn High School?")
 input("How do you handle a stressful situation in a busy classroom?")
 input("Can you share an example of how you work well with a team?")
