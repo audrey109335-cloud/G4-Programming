7a02a9c5adf102cbdb0a9dca8d0c42a0057939c0
audrey109335 <audrey109335@mcsva.org>
2026-08-28 15:17:52 -0400

    .

---
+++ Interview.py
@@ -0,0 +1,13 @@
+"""
+Filename: interview.py
+Author: <Francisco, Audrey>
+Created: <08/28/2026>
+Instructor: Holtslander
+"""
+print("What is your name?")
+input("_____")
+print("What made you want to work at Osbourn High School?")
+print("How do you handle a stressful situation in a busy classroom?")
+print("Can you share an example of how you work well with a team?")
+print("What is one fun fact about yourself that you would like our students to know?")
+print("Why do you think you are a great fit for our school community?")
