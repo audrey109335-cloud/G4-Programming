bdfe4547dbffed7d997fa452bc7a68added512cb
audrey109335 <audrey109335@mcsva.org>
2026-09-01 14:53:28 -0400

    .

---
+++ madlib.py
@@ -0,0 +1,38 @@
+"""
+Filename: madlibs.py
+Author: <Francisco, Audrey>
+Created: <09/01/2026>
+Instructor: Burgess
+"""
+w1=input("Name")
+w2=input("Adjective")
+w3=input("Name")
+w4=input("Verb")
+w5=input("Adjective")
+w6=input("Noun")
+w7=input("Adjective")
+w8=input("Noun")
+w9=input("Exclamation")
+w10=input("Plural noun")
+w11=input("Verb ending in -ing")
+w12=input("Adverb")
+w13=input("Adjective")
+w14=input("Noun")
+w15=input("Verb")
+w16=input("Noun")
+w17=input("Adjective")
+w18=input("Verb ending in -ed")
+w19=input("Adverb")
+w20=input("Noun")
+
+print(f"My name is {w1}, and yesterday my {w2} friend {w3} invited me to {w4}."
+      f"We walked in the {w5}{w6} when suddenly, a {w7} {w8} jumped out!"
+      f"I screamed, {w9}!"
+      f"This caused {w10} to start {w11} {w12} all around us."
+      f"I grabbed a {w13} {w14} while my friend tried to {w15} behind a {w16}."
+      f"We felt very {w17} until the chaos finally {w18} {w19} into the {w20}.")
+
+
+
+
+
