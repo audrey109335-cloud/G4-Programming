086901fc9988b408ef1bd51264eebe99e78a20e6
audrey109335 <audrey109335@mcsva.org>
2026-09-03 15:17:50 -0400

    .

--- Interview.py
+++ Interview.py
@@ -2,12 +2,21 @@
 Filename: interview.py
 Author: <Francisco, Audrey>
 Created: <08/28/2026>
-Instructor: Holtslander
+Instructor: Burgess
 """
-print("What is your name?")
-input("_____")
-print("What made you want to work at Osbourn High School?")
-print("How do you handle a stressful situation in a busy classroom?")
-print("Can you share an example of how you work well with a team?")
-print("What is one fun fact about yourself that you would like our students to know?")
-print("Why do you think you are a great fit for our school community?")
+print("Welcome to the Interview!")
+w1=input("What is your name?\n")
+print("Nice to Meet you!\n")
+w2=input("What made you want to work at Osbourn High School?\n")
+print("That's nice to hear!\n")
+w3=input("How do you handle a stressful situation in a busy classroom?\n")
+print("That is a very good example, nice!\n")
+w4=input("Can you share an example of how you work well with a team?\n")
+print("That sounds like good sportsmanship!\n")
+w5=input("What is one fun fact about yourself that you would like our students to know?\n")
+print("That would be great for the students to know!\n")
+w6=input("Why do you think you are a great fit for our school community?\n")
+print("That sounds like a great position for you!\n\n"
+      "Thank you for your time for this interview, will get back to you soon, Bye!!!\n")
+print(f"Summary: {w1}, {w2}, {w3}, {w4}, {w5}, {w6}")
+
