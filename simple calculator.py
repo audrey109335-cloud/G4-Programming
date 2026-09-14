07658080449749aac1fc8951d8e015a73315e9fd
audrey109335 <audrey109335@mcsva.org>
2026-09-14 15:20:23 -0400

    .

---
+++ simple calculator.py
@@ -0,0 +1,21 @@
+"""
+Filename: simple_calculator.py
+Author: <Francisco, Audrey>
+Created: <03/06/2026>
+Instructor: Burgess
+"""
+print("Welcome to the Four-Function Calculator!")
+print("This program will take two whole numbers and calculate the sum, difference, product, and quotient.")
+w1=input("Enter your first whole number, like 4 or 8, not 4.2 or 8.7")
+num1=int(w1)
+w2=input("Enter your second whole number, like 4 or 8, not 4.2 or 8.7")
+num2=int(w2)
+sum=num1+num2
+difference=num1-num2
+product=num1*num2
+quotient=num1/num2
+print(w1,"+",w2, "=", sum)
+print(w1,"-",w2, "=", difference)
+print(w1,"*",w2, "=", product)
+print(w1,"/",w2, "=", quotient)
+print("Thank you for using this program, Goodbye!")
